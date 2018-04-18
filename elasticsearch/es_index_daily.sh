80c0b483aca7b3038c730f88dff77ae7de49773f
jason lui <piml.lui@gmail.com>
2018-04-19 00:04:32 +0800

    Update es_index_daily.sh

--- elasticsearch/es_index_daily.sh
+++ elasticsearch/es_index_daily.sh
@@ -13,7 +13,7 @@ es_url="http://172.21.2.40:9200/"
 index_keywords=("application" "nginx_frontend" "nginx_backend")
 
 # print current segments info
-echo `########## start ########` >> ${es_index_log}
+echo '########## start ########' >> ${es_index_log}
 echo `date +"%Y-%m-%d"` >> ${es_index_log}
 curl -s "${es_url}_cat/nodes?v&h=name,segments.count,segments.memory,segments.index_writer_memory,segments.version_map_memory,segments.fixed_bitset_memory" >> ${es_index_log}
 
