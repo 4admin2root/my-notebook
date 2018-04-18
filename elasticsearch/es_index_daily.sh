eacff782c5bfb70cb0bdde1df6ac438b5a07196e
jason <zjlv@thoughtworks.com>
2018-04-18 23:53:28 +0800

    test

--- elasticsearch/es_index_daily.sh
+++ elasticsearch/es_index_daily.sh
@@ -6,22 +6,29 @@ es_index_log=/tmp/es_index.log
 
 yesterday=`date -d "1  days ago"  "+%Y.%m.%d"`
 
-daysago=`date -d "120  days ago"  "+%Y.%m.%d"`
+daysago=`date -d "108  days ago"  "+%Y.%m.%d"`
 
 es_url="http://172.21.2.40:9200/"
 
 index_keywords=("application" "nginx_frontend" "nginx_backend")
 
 # print current segments info
+echo `########## start ########` >> ${es_index_log}
 echo `date +"%Y-%m-%d"` >> ${es_index_log}
 curl -s "${es_url}_cat/nodes?v&h=name,segments.count,segments.memory,segments.index_writer_memory,segments.version_map_memory,segments.fixed_bitset_memory" >> ${es_index_log}
 
-
+# close indexes
 for i in ${index_keywords[@]}; do
 echo "close index: ${i}" >> ${es_index_log}
-curl -XPOST -s ${es_url}${i}*${daysago}/_close -d '' >> ${es_index_log}
-sleep 1
+curl -XPOST -s "${es_url}${i}*${daysago}/_close" -d '' >> ${es_index_log}
+sleep 10
 done
 
+# merge indexes
+for i in ${index_keywords[@]}; do
+echo "merge index: ${i}" >> ${es_index_log}
+curl -XPOST -s "${es_url}${i}*${yesterday}/_forcemerge?max_num_segments=1" -d '' >> ${es_index_log}
+sleep 100
+done
 
 curl -s "${es_url}_cat/nodes?v&h=name,segments.count,segments.memory,segments.index_writer_memory,segments.version_map_memory,segments.fixed_bitset_memory" >> ${es_index_log}
